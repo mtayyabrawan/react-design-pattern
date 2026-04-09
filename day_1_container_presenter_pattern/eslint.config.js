c7b631df9047128c641afcd007f2de46b15eb9b9
mtayyabrawan <mtayyabawan1918@gmail.com>
2026-04-09 13:45:04 +0500

    day 1: messy way

--- day_1_container_presenter_pattern/eslint.config.js
+++ day_1_container_presenter_pattern/eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -11,7 +10,6 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
---
+++ day_1_container_presenter_pattern/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from "./messy-way/components/UserProfile.jsx";
+
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <UserProfile userId="1"/>
+    </div>
+  );
+}
+
+export default App;
